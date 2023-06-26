ca4a2daaef0c68fd8e2ea6b9c366a9daec976fa4
shetaruchit <91286058+shetaruchit@users.noreply.github.com>
2023-06-26 14:23:30 +0530

    footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,50 @@
 import React from 'react'
 import "./Footer.css"
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import PauseCircleOutlineIcon from "@mui/icons-material/PauseCircleOutline";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import Grid from '@mui/material/Grid';
+import Slider from '@mui/material/Slider';
+//image of footer footer song
+import OneDay from "../image/OneDay.jpg"
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am footer</h1>
+       <div className="footer__left">
+        <img className="footer__albumLogo" src={OneDay} alt="" />
+        <div className="footer__songInfo">
+          <h4>One Day</h4>
+          <p>Nic D</p>
+        </div>
+       </div>
+
+       <div className="footer__center">
+        <ShuffleIcon className="footer__green"/>
+        <SkipPreviousIcon className="footer__icon"/>
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
+        <SkipNextIcon className="footer__icon"/>
+        <RepeatIcon className="footer__green"/>
+
+       </div>
+
+       <dov className="footer__right">
+       <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider  />
+          </Grid>
+        </Grid>
+       </dov>
     </div>
   )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -6,7 +6,7 @@ export const initialState = {
 
     //remove it
     // token:null
-    // token:"BQCZe6cbMKvQ8FmHRP3E5PVjf13bwyk8AW5AuNSAeV_fWKJ5YOAl2PaNiqbgT_7WUpzCBLudIIQHpdFIRRH0ozlqb53R-ho7C459MCPOZu5MExkbOyj8deMp6tgLB0mJFTQa3yXiXGefdudAQINaeXyZHZwFFaWV3KmA3RzEoAwuNwog8OQgJd7Pdlz6jlALpr1BrGmA6Kxm5s7qVVPT"
+    token:"BQCZe6cbMKvQ8FmHRP3E5PVjf13bwyk8AW5AuNSAeV_fWKJ5YOAl2PaNiqbgT_7WUpzCBLudIIQHpdFIRRH0ozlqb53R-ho7C459MCPOZu5MExkbOyj8deMp6tgLB0mJFTQa3yXiXGefdudAQINaeXyZHZwFFaWV3KmA3RzEoAwuNwog8OQgJd7Pdlz6jlALpr1BrGmA6Kxm5s7qVVPT"
 };
 
 const reducer = (state, action) => {
