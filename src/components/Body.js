8e7abdeb714f6aaba7471bbdf557e09e7ecdc2af
shetaruchit <91286058+shetaruchit@users.noreply.github.com>
2023-07-06 19:53:19 +0530

    2.0

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,37 +6,44 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { useDataLayerValue } from '../DataLayer'
 import SongRow from './SongRow';
+import { green } from '@mui/material/colors';
 
 function Body(spotify) {
 
-  const[{running_playlist},dispatch] = useDataLayerValue();
+  const [{ running_playlist }, dispatch] = useDataLayerValue();
 
   return (
     <div className='body'>
-        <Header spotify={spotify}/>
-
+      
+      <div className="body__uper">
+        <Header spotify={spotify} />
         <div className="body__info">
           <img src={running_playlist?.images[0].url} alt="" />
           <div className="body__infoText">
-            <strong>PLAYLIST</strong>
+            <strong>Playlist</strong>
             <h2>Eccentric</h2>
             <p>{running_playlist?.description}</p>
           </div>
         </div>
+      </div>
+
+      <div className="body__lower">
 
         <div className="body__songs">
           <div className="body__icons">
-              <PlayCircleFilledIcon className='body__shuffle'/>
-              <FavoriteIcon fontSize='large'/>
-              <MoreHorizIcon/>  
+            <PlayCircleFilledIcon sx={{ color: green[500] }} className='body__shuffle' />
+            <FavoriteIcon sx={{ color: green[500] }} fontSize='large' />
+            <MoreHorizIcon />
           </div>
 
           {/* List of songs */}
-          {running_playlist?.tracks.items.map(item=>(
-          
-            <SongRow track={item.track}/>
+          {running_playlist?.tracks.items.map(item => (
+
+            <SongRow track={item.track} />
           ))}
         </div>
+      </div>
+
     </div>
   )
 }
